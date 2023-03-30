df97697304a9e2196a680f206bf87b12ade83390
Felipe Carrillo <felipe-angel.carrillocastillo.external@atos.net>
2023-03-30 12:49:00 -0600

    Add project files.

---
+++ BasicSecurityASP/Services/BeerService.cs
@@ -0,0 +1,21 @@
+﻿using BasicSecurityASP.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BasicSecurityASP.Services
+{
+    public class BeerService: iBeerService
+    {
+        string path = "C:\\Users\\a892976\\source\\repos\\BasicSecurityASP\\BasicSecurityASP\beers.json";
+    
+        public async Task<List<Beer>> Get()
+        {
+            string content = await File.ReadAllTextAsync(path);
+            var beers = JsonSerializer.Deserialize<List<Beer>>(content);
+            return beers;
+        }
+            
+    }
+}
---
+++ BasicSecurityASP/Services/iBeerService.cs
@@ -0,0 +1,11 @@
+﻿using BasicSecurityASP.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BasicSecurityASP.Services
+{
+    public interface iBeerService
+    {
+        public Task<List<Beer>> Get(); 
+    }
+}
